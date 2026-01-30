00addbeed28aee34d87da27cd28881e9194db244
Thiago Lima <tlima7601@gmail.com>
2026-01-30 11:26:13 -0300

    Responsivo de celular feito

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import { HashLink } from 'react-router-hash-link';
 import { GiHamburger } from "react-icons/gi";
 import { FaWhatsapp } from "react-icons/fa";
-import { FaShoppingBag } from "react-icons/fa";
 import { useState } from 'react';
 import './Navbar.css';
 import Sidebar from '../sidebar/Sidebar';
@@ -36,9 +35,6 @@ const Navbar = () => {
                 <HashLink className="btn btn-pedir" smooth to="/#cardapio">
                     <FaWhatsapp size={20} />Peça Agora
                 </HashLink>
-                <button type='button' onClick={handleToggleMenu} className='sacola'>
-                    <FaShoppingBag color='var(--primary)' size={35} />
-                </button>
             </div>
             {menuOpen && <div className="overlay" onClick={handleToggleMenu} />}
             <Sidebar isOpen={menuOpen} />
